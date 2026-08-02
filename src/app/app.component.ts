b42811dd9b8053d0b981be5fead2570a79ac4f3f
Moitreya Chaudhury <moitreya.chaudhury@gmail.com>
2026-08-02 21:07:09 +0530

    output decorators and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy_users';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent  {
+export class AppComponent {
   protected readonly title = signal('first-angular-app');
   users = DUMMY_USERS;
+
+  onSelectUser(user: any) {
+    this.users = this.users.map((u: any) => ({
+      ...u,
+      selected: u.id === user.id,
+    }));
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, Output,EventEmitter} from '@angular/core';
+import { NgClass } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 
 export class UserComponent  {
   @Input({required: true}) selectedUser!: any;
+  @Output() selectUser = new EventEmitter<string>();
   get imagePath(): string {
     return `users/${this.selectedUser.avatar}`;
   }
   onSelectUser(){
-  
+    this.selectUser.emit(this.selectedUser);
   }
 }
